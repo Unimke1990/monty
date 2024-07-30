881c0ae3c982ae5b80a2c7a438169160357653e5
Unimke1990 <132392244+Unimke1990@users.noreply.github.com>
2024-07-30 10:05:55 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * f_queue - print first node of queue
+ * f_queue - print first node of the queue
  * @head: first node of queue (unused)
  * @counter: line counter (unused)
  *
